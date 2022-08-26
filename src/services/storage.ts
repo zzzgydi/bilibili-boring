dcba0a264d61c646b842bc6eb249a838e1949d91
GyDi <segydi@foxmail.com>
2022-08-26 18:53:18 +0800

    feat: 优化storage逻辑

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,58 +1,95 @@
-export function storage<T = any>(
-  key: string
-): { set: (data?: T) => void; get: () => T | undefined | null };
+type Getter<T> = () => T | undefined | null;
+type NotNoneGetter<T> = () => T;
+type Return<T, G> = { set: (data?: T) => void; get: G };
 
+type Options<T> = {
+  isJSON?: boolean;
+  cacheTime?: number;
+  validate?: (data: any) => boolean;
+  stringify?: (data: any) => string;
+  parse?: (data: string) => T;
+};
+type OptionsWithDefault<T> = Options<T> & { defaultValue: T };
+
+export function storage<T = any>(key: string): Return<T, Getter<T>>;
 export function storage<T = any>(
   key: string,
-  defaultValue: T
-): { set: (data?: T) => void; get: () => T };
+  options: OptionsWithDefault<T>
+): Return<T, NotNoneGetter<T>>;
 
-export function storage<T = any>(
-  key: string,
-  defaultValue: T,
-  validate: (data: any) => boolean
-): { set: (data?: T) => void; get: () => T };
+export function storage<T = any>(key: string, options?: OptionsWithDefault<T>) {
+  const {
+    defaultValue,
+    isJSON = true,
+    cacheTime = 0,
+    validate,
+    stringify,
+    parse,
+  } = options ?? {};
 
-export function storage<T = any>(
-  key: string,
-  defaultValue?: T,
-  validate?: (data: any) => boolean
-) {
+  let cache: T | undefined;
+  let updated = 0;
+
   return {
     set: (data?: T) => {
+      cache = data;
+      updated = Date.now();
+
       try {
         if (data == null) {
           localStorage.removeItem(key);
           return;
         }
-        localStorage.setItem(key, JSON.stringify(data));
+        const value = isJSON
+          ? JSON.stringify(data)
+          : stringify
+          ? stringify(data)
+          : (data as unknown as string);
+        localStorage.setItem(key, value);
       } catch (e) {
         console.error(e);
       }
     },
     get: () => {
+      if (cacheTime > 0 && Date.now() - updated < cacheTime) {
+        return cache;
+      }
       try {
+        updated = Date.now();
         const dataStr = localStorage.getItem(key);
-        if (!dataStr) return defaultValue;
+        if (!dataStr) return (cache = defaultValue);
+        const data = isJSON
+          ? (JSON.parse(dataStr) as T)
+          : parse
+          ? parse(dataStr)
+          : (dataStr as unknown as T);
 
-        const data = JSON.parse(dataStr) as T;
-        if (!validate || (validate && validate(data))) return data;
+        if (!validate || (validate && validate(data))) return (cache = data);
       } catch (e) {
         console.log(e);
       }
-      return defaultValue;
+      return (cache = defaultValue);
     },
   };
 }
 
-export const SVideoList = storage<IVideoItem[]>(
-  "boring-list",
-  [],
-  Array.isArray
-);
-export const SVideoListLen = storage<number>("boring-list-len", 0);
-export const SVideoHistoryList = storage<string[]>(
-  "boring-history",
-  [],
-  Array.isArray
-);
+export const SVideoList = storage<IVideoItem[]>("boring-list", {
+  defaultValue: [] as IVideoItem[],
+  cacheTime: 10000,
+  validate: Array.isArray,
+});
+export const SVideoHistoryList = storage<string[]>("boring-history", {
+  defaultValue: [],
+  cacheTime: 10000,
+  validate: Array.isArray,
+});
+export const SWhitelistText = storage<string>("boring-whitelist", {
+  defaultValue: "",
+  isJSON: false,
+  cacheTime: 10000,
+});
+export const SBlacklistText = storage<string>("boring-blacklist", {
+  defaultValue: "",
+  isJSON: false,
+  cacheTime: 10000,
+});
